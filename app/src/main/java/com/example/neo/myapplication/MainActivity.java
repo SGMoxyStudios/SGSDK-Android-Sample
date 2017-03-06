eb5ed9b65d4a5e4f21e3e8f9fd4a4e31d62a63b8
Neo Hsu <neoxuisme@gmail.com>
2017-03-06 17:39:59 +0800

    I6 set cookie

--- app/src/main/java/com/example/neo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/neo/myapplication/MainActivity.java
@@ -1,31 +1,29 @@
 package com.example.neo.myapplication;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebResourceRequest;
-import android.net.http.*;
 import android.util.Log;
 import android.webkit.ValueCallback;
-import java.net.*;
-import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    //String URL_Init = "http://gameapi.smartgamesltd.com/init";
-    String URL_Init = "http://192.168.201.247:12000/init";
+    //String Host = "http://gameapi.smartgamesltd.com/";
+    //String URL_Init = "init";
     //String GameKey = "d7e73390-d7fd-11e6-9074-21c247f06802";
-    String GameKey = "0e0c3020-dd2f-11e6-ae80-6ffc5013c85a";
     //String AppSecret = "d7d7c810-d7ee-11e6-9e86-db4f79aeee86";
-    String AppSecret = "f84b3010-dd2e-11e6-ae80-6ffc5013c85a";
     //String PrivateKey = "azlxEUasGMOf1VoygA9Jvta76pG8QnoC";
+    String Host = "http://192.168.201.155:12000/";
+    String URL_Init = "init";
+    String GameKey = "0e0c3020-dd2f-11e6-ae80-6ffc5013c85a";
+    String AppSecret = "f84b3010-dd2e-11e6-ae80-6ffc5013c85a";
     String PrivateKey = "KjUQMi2FCMeddTeDqc5gkUYbn97xAuKQ";
     WebView mWebView = null;
+    Map<String, String> webViewCookie = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,9 +92,9 @@ public class MainActivity extends AppCompatActivity {
 
     //1.
     public void Init(String gamekey, String appsecret, String privatekey) {
-        mWebView.loadUrl(String.format("%s?gamekey=%s&appsecret=%s&privatekey=%s", URL_Init, gamekey, appsecret, privatekey));
-        /*
-        String url = String.format("%s?gamekey=%s&appsecret=%s&privatekey=%s&channel=googleplay", URL_Init, gamekey, appsecret, privatekey);
+        //mWebView.loadUrl(String.format("%s?gamekey=%s&appsecret=%s&privatekey=%s", Host + URL_Init, gamekey, appsecret, privatekey));
+
+        String url = String.format("%s?gamekey=%s&appsecret=%s&privatekey=%s&channel=googleplay", Host + URL_Init, gamekey, appsecret, privatekey);
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -104,64 +102,36 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         Log.d("Response is ", response);
+                        Login();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.d("Response error ", error.toString());
             }
-        });
+        }) {
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                if (response.headers.containsKey("Set-Cookie")) {
+                    webViewCookie.clear();
+                    webViewCookie.put("cookie", response.headers.get("Set-Cookie"));
+                }
+
+                return super.parseNetworkResponse(response);
+            }
+        };
 
         MySingleton.getInstance(this).addToRequestQueue(stringRequest);
-*/
-        /*
-        try {
-            URL url = null;
-            String response = null;
-            String parameters = String.format("gamekey=%s&appsecret=%s&privatekey=%s", gamekey, appsecret, privatekey);
-            url = new URL(URL_Init);
-            //create the connection
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            //set the request method to GET
-            connection.setRequestMethod("GET");
 
-            //get the output stream from the connection you created
-            OutputStreamWriter request = new OutputStreamWriter(connection.getOutputStream());
-            //write your data to the ouputstream
-            request.write(parameters);
-            request.flush();
-            request.close();
-            String line = "";
-            //create your inputsream
-            InputStreamReader isr = new InputStreamReader(connection.getInputStream());
-            //read in the data from input stream, this can be done a variety of ways
-            BufferedReader reader = new BufferedReader(isr);
-            StringBuilder sb = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            //get the string version of the response data
-            response = sb.toString();
-            //do what you want with the data now
-
-            //always remember to close your input and output streams
-            isr.close();
-            reader.close();
-            Log.d("req : ", response);
-        } catch (IOException e) {
-            Log.e("HTTP GET:", e.toString());
-        }*/
     }
 
     //2.
     public void Login() {
-        mWebView.loadUrl("http://gameapi.smartgamesltd.com/login");
+        mWebView.loadUrl(Host + "login", webViewCookie);
     }
 
     //3.
     public void GetOpenID() {
-        mWebView.loadUrl("http://gameapi.smartgamesltd.com/openid");
+        mWebView.loadUrl(Host + "openid", webViewCookie);
     }
 }
